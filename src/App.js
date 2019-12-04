af218a09e1f3d8384c58fd6f3e82c8669a98dc95
Ozal Khan <ozalkhan196@gmail.com>
2019-12-04 13:57:18 -0500

    added styles to center login/signup/resetform

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from "react";
 import { LoginForm } from "./components";
+import styled from "styled-components";
+
+const ContactBanner = styled.h3`
+  text-align: center;
+  font-family: Arvo, "Open Sans", Arial, sans-serif;
+`;
+
+const AppContainer = styled.div`
+  margin-top: 4%;
+`;
 
 const App = () => {
   return (
-    <div>
-      <div>
-        <LoginForm />
-      </div>
+    <AppContainer>
+      <LoginForm />
       <h4>
-        Need help? No problem! Email support@homebinder.com or Call 800-377-6915
+        <ContactBanner>
+          Need help? No problem! Email support@homebinder.com or Call
+          800-377-6915
+        </ContactBanner>
       </h4>
-    </div>
+    </AppContainer>
   );
 };
 
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,9 +4,12 @@ import styled from "styled-components";
 const FormContainer = styled.div`
   display: flex;
   align-items: center;
+  flex-direction: column;
   padding: 5px 10px;
   background: papayawhip;
   color: palevioletred;
+  width: 50%;
+  margin: auto;
 `;
 
 const Title = styled.h1`
@@ -15,12 +18,25 @@ const Title = styled.h1`
   color: palevioletred;
 `;
 
+const ImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  color: #333;
+  background-color: #f5f5f5;
+  border-color: #ddd;
+  border-top-right-radius: 3px;
+  border-top-left-radius: 3px;
+`;
+
 const Image = styled.img``;
 
 const LoginForm = () => {
   return (
     <FormContainer>
-      <Image src="images/logo.png" />
+      <ImageContainer>
+        <Image src="images/logo.png" />
+      </ImageContainer>
       <Title>Some title stuff</Title>
     </FormContainer>
   );
